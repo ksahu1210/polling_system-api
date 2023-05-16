aa6609d93b32b31b2f43888073774b6f16cefd55
ksahu1210 <122907758+ksahu1210@users.noreply.github.com>
2023-05-16 22:28:16 +0530

    Add files via upload

---
+++ config/mongoose.js
@@ -0,0 +1,20 @@
+// configuration for creating database connection
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/polling_system',
+{
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    family: 4,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
+
+db.once('open', function(){
+    console.log('Connected to Database :: MongoDB');
+});
+
+module.exports = db;
